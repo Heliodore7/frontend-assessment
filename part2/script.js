a803387ff82ebcbc3d3b57b250ece18db3d8ec06
Heliodore <ndahheliodore@gmail.com>
2023-12-30 14:22:27 +0100

    N'DAH Heliodore

---
+++ part2/script.js
@@ -0,0 +1,6 @@
+/* Ecrivez un script qui demande a l'utilisateur son nom prenom et sa date de naissance
+let Nom = String(prompt("Entrer votre Nom"))
+let Prenom = String(prompt("Entrer votre Prenoms"))
+let Age = Number(prompt("Entrer votre Année de Naissance"))
+
+alert("Hello" ++ "Nom +""Prenom"+"Age-2023")
